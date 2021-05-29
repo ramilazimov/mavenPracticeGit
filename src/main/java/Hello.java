766930ee0f0dd723e36570e76b59fd8b34f0e497
ramilazimov <79541318+ramilazimov@users.noreply.github.com>
2021-05-28 23:01:02 -0400

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("developing hello future");
+        System.out.println("github repo is already publish");
+        System.out.println("doing more work");
     }
 }
